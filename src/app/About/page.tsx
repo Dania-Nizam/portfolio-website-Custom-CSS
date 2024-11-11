b3469e5a44b391ba99b984bf7d37cdcd76799098
Dania-Nizam <almirashah35@gmail.com>
2024-11-10 20:35:45 -0800

    change

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-
+import Skills from '../skills/page'
 const About = () => {
   return (
     <div>
@@ -22,43 +22,3 @@ const About = () => {
 
 export default About
 
-export const Skills=()=>{
-  return(
-    <div>
-      
-        <h1 className='heading'>MY SKILLS</h1>
-      <div>
-        <h2 className='skill-h2'>HTML</h2>
-        <div className='bar'></div>
-      </div>
-
-      <div>
-        <h2 className='skill-h2'>CSS</h2>
-        <div className='bar1'></div>
-      </div>
-
-      <div>
-        <h2 className='skill-h2'>JAVASCRIPT</h2>
-        <div className='bar2'></div>
-      </div>
-
-
-      <div>
-        <h2 className='skill-h2'>TYPESCRIPT</h2>
-        <div className='bar3'></div>
-      </div>
-
-      <div>
-        <h2 className='skill-h2'>NEXT JS</h2>
-        <div className='bar4'></div>
-      </div>
-
-      <div>
-        <h2 className='skill-h2'>FIGMA</h2>
-        <div className='bar5'></div>
-      </div>
-    </div>
-
-    
-  )
-}
---
+++ src/app/skills/page.tsx
@@ -0,0 +1,42 @@
+export const Skills=()=>{
+    return(
+      <div>
+        
+          <h1 className='heading'>MY SKILLS</h1>
+        <div>
+          <h2 className='skill-h2'>HTML</h2>
+          <div className='bar'></div>
+        </div>
+  
+        <div>
+          <h2 className='skill-h2'>CSS</h2>
+          <div className='bar1'></div>
+        </div>
+  
+        <div>
+          <h2 className='skill-h2'>JAVASCRIPT</h2>
+          <div className='bar2'></div>
+        </div>
+  
+  
+        <div>
+          <h2 className='skill-h2'>TYPESCRIPT</h2>
+          <div className='bar3'></div>
+        </div>
+  
+        <div>
+          <h2 className='skill-h2'>NEXT JS</h2>
+          <div className='bar4'></div>
+        </div>
+  
+        <div>
+          <h2 className='skill-h2'>FIGMA</h2>
+          <div className='bar5'></div>
+        </div>
+      </div>
+  
+      
+    )
+  } 
+  
+   export default Skills;
